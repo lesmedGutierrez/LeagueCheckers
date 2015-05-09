ebd253c3576fb77388488bfea914c156beebbf9b
lesmedGutierrez <lesmedgutiar.16@gmail.com>
2015-05-08 20:25:22 -0600

    Se agrego la clase Board

---
+++ Chess/Models/Board.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Chess.Helpers;
+
+namespace Chess.Models
+{
+    public class Board
+    {
+        public List<properties> pieces = new List<properties>();
+        public string currentColorTurn = "";
+
+
+
+        public bool canJump(properties piece)
+        {
+            return piece.name == "pawn" ? canPawnJump(piece) : canCrownJump(piece);
+        }
+
+
+
+        private bool canPawnJump(properties piece)
+        {
+            int col = piece.col;
+            int row = piece.row;
+
+            if (piece.name == "pawn" && currentColorTurn == "White")
+            {
+
+                if (piece.row > 1 && piece.col > 1 &&
+                   getPiece(row - 1, col - 1).color == "Black" && getPiece(row - 2, col - 2).color == "Blank") // check jump to the left
+                {
+                    return true;
+                }
+                if (col < 6 && row > 1 &&
+                   getPiece(row - 1, col + 1).color == "Black" && getPiece(row - 2, col + 2).color == "Blank") // check jump to the right
+                {
+                    return true;
+                }
+            }
+            else if (piece.name == "pawn" && currentColorTurn == "Black")
+            {
+                if (col > 1 && row < 6 &&
+                   getPiece(row + 1, col - 1).color == "White" && getPiece(row + 2, col - 2).color == "Blank") // check jump to the left
+                {
+                    return true;
+                }
+                if (col < 6 && row < 6 &&
+                   getPiece(row + 1, col + 1).color == "White" && getPiece(row + 2, col + 2).color == "Blank") // check jump to the right
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        private bool canCrownJump(properties piece)
+        {
+            int col = piece.col;
+            int row = piece.row;
+
+            string opponent;
+            if (piece.name == "crown")
+            {
+                opponent = "Black";
+            }
+            else // DARKKING
+            {
+                opponent = "White";
+            }
+            if (col > 1 && row > 1 &&
+               getPiece(row - 1, col - 1).color == opponent && getPiece(row - 2, col - 2).color == "Blank") // check jump to the left
+            {
+                return true;
+            }
+            if (col < 6 && row > 1 &&
+               getPiece(row - 1, col + 1).color == opponent && getPiece(row - 2, col + 2).color == "Blank") // check jump to the right
+            {
+                return true;
+            }
+            if (col > 1 && row < 6 &&
+               getPiece(row + 1, col - 1).color == "White" && getPiece(row + 2, col - 2).color == "Bank") // check jump to the left
+            {
+                return true;
+            }
+            if (col < 6 && row < 6 &&
+               getPiece(row + 1, col + 1).color == "White" && getPiece(row + 2, col + 2).color == "Blank") // check jump to the right
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        public bool hasJump()
+        {
+            for (int i = 0; i < pieces.Count; i++)
+            {
+                string pieceColor = pieces[i].color;
+
+                if (pieceColor == currentColorTurn)
+                {
+                    if (canJump(pieces[i]))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+
+
+        public void removePiece(string id)
+        {
+
+            foreach (properties item in pieces)
+            {
+                if (item.id == id)
+                {
+                    pieces.Remove(item);
+                    break;
+                }
+            }
+        }
+
+        public properties getPiece(int row, int col)
+        {
+            for (int i = 0; i < pieces.Count; i++)
+            {
+                if (pieces[i].row == row && pieces[i].col == col)
+                {
+                    return pieces[i];
+                }
+            }
+            return new properties();
+        }
+
+
+
+    }
+}
